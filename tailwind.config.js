e99732e86dcd9ad515244d4dd08ffd190401cca8
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-06 15:55:45 +0200

    setup colors done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
     purge: ['./pages/**/*.js', './components/**/*.js'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            backgroundColor: (theme) => ({
+                ...theme('colors'),
+                'primary-dark': '#0A1225',
+                'primary-blue': '#143CCB'
+            })
+        }
     },
     variants: {
         extend: {}
