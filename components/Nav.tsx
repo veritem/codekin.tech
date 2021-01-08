c1f9e88d309125c864ab7214cca175f2dad9abe6
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-08 08:38:14 +0200

    settinging up dark done

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,26 @@
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+
 export default function Nav(): React.ReactElement {
+    const [mounted, setMounted] = useState<boolean>(false)
+
+    const { theme, setTheme } = useTheme()
+
+    useEffect(() => setMounted(true), [])
+
     return (
         <nav className="flex justify-between items-center">
             <div>
                 <h2>CodeKin.tech</h2>
             </div>
             <div className=" flex justify-between items-center">
+                {mounted && (
+                    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+                        {theme === 'dark' ? 'change to light theme' : 'change to dark theme'}
+                    </button>
+                )}
                 <p>theme</p>
+
                 <p>About us</p>
             </div>
         </nav>
