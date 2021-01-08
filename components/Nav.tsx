a1ae6da19e6d83cc4ce8f466e7123d88c399f3f8
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-08 13:33:23 +0200

    Fixed svg icons

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -37,17 +37,24 @@ export default function Nav(): React.ReactElement {
                             width="24"
                             height="24"
                             fill="currentColor"
-                            // stroke="currentColor"
                             className="text-gray-800 dark:text-gray-200">
                             {theme === 'dark' ? (
                                 <>
-                                    <path fill="none" d="M0 0h24v24H0z" />
-                                    <path d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z" />
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                                    />
                                 </>
                             ) : (
                                 <>
-                                    <path fill="none" d="M0 0h24v24H0z" />
-                                    <path d="M10 7a7 7 0 0 0 12 4.9v.1c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2h.1A6.979 6.979 0 0 0 10 7zm-6 5a8 8 0 0 0 15.062 3.762A9 9 0 0 1 8.238 4.938 7.999 7.999 0 0 0 4 12z" />
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                                    />
                                 </>
                             )}
                         </svg>
