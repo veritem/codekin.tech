0fe856f38daa243285b228e4639e6fb5cbb2d3f4
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-21 15:25:12 +0200

    removed the favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '@/lib/gtag'
 
 export default class MyDocument extends Document {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -7,6 +8,21 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta content="#ffffff" name="theme-color" />{' '}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                    });
+          `
+                        }}></script>
                 </Head>
                 <body className=" dark:bg-black dark:text-black">
                     <Main />
