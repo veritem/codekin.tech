9f84744d25b378f416493b35f6256f736e0f19c8
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-28 21:52:38 +0200

    Footer styling

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav(): React.ReactElement {
     useEffect(() => setMounted(true), [])
 
     return (
-        <nav className="sticky-nav flex dark:bg-primary-dark bg-opacity-60  justify-around items-center pt-2 pb-2">
+        <nav className=" fixed w-full flex  bg-opacity-80    justify-around items-center pt-2 pb-2">
             <div>
                 <Link href="/">
                     <a className=" dark:text-white font-display capitalize font-bold text-xl">
