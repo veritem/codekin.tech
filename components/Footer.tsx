30d92c96dae594e60c3c0d14093f45b9ce164fd4
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-28 21:59:44 +0200

    Footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
 export default function Footer() {
     return (
-        <div className="border-t-2 border-gray-50">
-            <div className=" flex justify-between py-4">
+        <>
+            <div className="w-full border-t border-gray-100 dark:border-gray-800"></div>
+            <div className=" flex justify-between py-8">
                 <p className="text-gray-800 dark:text-gray-200">Github</p>
                 <p className="text-gray-800 dark:text-gray-200">About</p>
                 <p className="text-gray-800 dark:text-gray-200">Contact</p>
                 <p className="text-gray-800 dark:text-gray-200">&copy; 2020</p>
             </div>
-        </div>
+        </>
     )
 }
