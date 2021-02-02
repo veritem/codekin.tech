637f76892f7a43aa4cbf975695eca7671ae5ad53
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-02-02 23:22:29 +0200

    feature: added more tests

---
+++ tests/mdxComponent.test.ts
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import MdxComponent from '../components/MdxComponent'
+import renderer from 'react-test-renderer'
+
+// test('should render mdxComponent', () => {
+//     const tree = renderer.create(<MdxComponent />).toJSON()
+//     expect(tree).toMatchSnapshot()
+// })
