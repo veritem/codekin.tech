95195d10ff8c97a0d8f0d5fe5f97ddb7fc0f2d49
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-04-11 17:28:10 +0200

    feat: :chart_with_upwards_trend: feat
    improved the ui

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -6,7 +6,7 @@ const IndexLayout: NextPage = ({ children }) => {
         <section className="max-h-screen max-w-full">
             <Nav />
             <div className="w-full sm:w-full md:w-2/3 m-auto relative">
-                <main className="flex px-6 md:px-0 sm:px-8 justify-center content-start mt-20 mb-8 flex-col">
+                <main className="flex px-0 md:px-36 sm:px-8 sm:pr-8 justify-center content-center mt-20 mb-8 flex-col">
                     {children}
                 </main>
             </div>
