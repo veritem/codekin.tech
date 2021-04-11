2c64ef3af2457d43b54f7b895c1d2e0a5ff3d235
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-04-11 21:59:22 +0200

    feat: :iphone: fix
    fixed footer on small devices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     const YEAR = new Date().getFullYear()
     return (
         <section className="mb-8">
-            <div className="grid grid-cols-4 md:grid-cols-4 gap-8 sm:grid-cols-2 flex-wrap">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-8 sm:grid-cols-2 flex-wrap">
                 <p className="text-gray-800 dark:text-gray-200">
                     <Link href="https://github.com/codekinHQ">
                         <a>Github</a>
