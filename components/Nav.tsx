373b206530fba0e0a3ff80392f8a62bf34e7f807
veritem <mugaboverite@gmail.com>
2021-04-24 20:12:43 +0200

    feat: feat light mode

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 //TODO: Fix the Navbar
 export default function Nav(): React.ReactElement {
@@ -11,9 +11,9 @@ export default function Nav(): React.ReactElement {
     useEffect(() => setMounted(true), [])
 
     return (
-        <nav className="fixed pl-20 top-0 z-10 w-full py-2 bg-white border-b border-gray-400 dark:border-opacity-10 border-opacity-20 md:bg-opacity-70 dark:bg-primary dark:bg-opacity-40 filter-blur">
+        <nav className="fixed pl-20 bg-white top-0 z-10 w-full py-2 bg-white border-b border-gray-400 dark:border-opacity-10 border-opacity-20 md:bg-opacity-70 dark:bg-primary dark:bg-opacity-40 filter-blur">
             <Link href="/">
-                <a className="pl-8 md:pl-80 sm:pl-8 dark:text-white font-display  font-bold text-xl">
+                <a className="pl-8 md:pl-80 sm:pl-8 text-gray-900 dark:text-white font-display  font-bold text-xl">
                     codekin.tech
                 </a>
             </Link>
