5edef57f8b1eea213d2f46672e4c8ec497581a56
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-07-22 15:53:40 +0200

    chore: fix api

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -1,7 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getAllPosts } from '@/lib/posts'
+// import { getAllPosts } from '@/lib/posts'
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
 
-export default function Handler(_req: NextApiRequest, res: NextApiResponse) {
-    const posts = getAllPosts().filter((item) => (item as any).isPublished)
-    res.status(200).json(posts)
+export default (_req: NextApiRequest, res: NextApiResponse) => {
+    const dir = path.resolve('./posts')
+
+    const filenames = fs.readdirSync(dir)
+
+    const posts = filenames.map((name) => {
+        const fullname = path.join(process.cwd(), 'posts', name)
+        const fileContent = fs.readFileSync(fullname)
+        const { data } = matter(fileContent)
+        return data
+    })
+
+    let response = posts.filter((post) => post.isPublished)
+
+    res.status(200).json(response)
 }
