3884c5bba609ec0d08b5b85a30204794f2ee77c5
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-07-22 16:27:51 +0200

    chore: update api calls

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -1,10 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getAllPosts } from '@/lib/posts'
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
 
 export default (_req: NextApiRequest, res: NextApiResponse) => {
-    const posts = getAllPosts()
+    const dir = path.resolve('./posts')
 
-    let response = posts.filter((post) => (post as any).isPublished)
+    const filenames = fs.readdirSync(dir)
+
+    const posts = filenames.map((name) => {
+        const fullname = path.join(process.cwd(), 'posts', name)
+        const fileContent = fs.readFileSync(fullname)
+        const { data } = matter(fileContent)
+        return data
+    })
+
+    const response = posts.filter((post) => post.isPublished)
 
     res.status(200).json(response)
 }
